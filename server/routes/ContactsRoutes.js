7d8a2dd8ffd25ef70ad70d9acd0149944a6b96d8
Sanjeevi Rajan <>
2025-08-21 11:19:46 +0530

    Added all required fields and Added Search Controller and Add Event to Existing Contact Controller

--- server/routes/ContactsRoutes.js
+++ server/routes/ContactsRoutes.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { CreateContact, GetContacts, UpdateContact, DeleteContact } from "../controllers/ContactControllers.js";
+import {
+    CreateContact,
+    GetContacts,
+    UpdateContact,
+    DeleteContact,
+    SearchContacts,
+    AddEventToExistingContact,
+} from "../controllers/ContactControllers.js";
 import { upload, UploadImage } from "../controllers/PhotoControllers.js";
 
 const router = express.Router();
-router.post("/create-contact", CreateContact);
 router.get("/contacts", GetContacts);
+router.get("/search-contact", SearchContacts);
+router.post("/create-contact", CreateContact);
+router.post("/upload-contact/", upload.single("image"), UploadImage);
 router.put("/update-contact/:id", UpdateContact);
 router.delete("/delete-contact/:id", DeleteContact);
-router.post("/upload-contact/", upload.single('image'), UploadImage);
+router.post("/add-event-existing-contact/:contactId", AddEventToExistingContact);
 
 export default router;
