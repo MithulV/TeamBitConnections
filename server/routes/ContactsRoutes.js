fdf0c80e3d5012624cfddd3d2046c234b9e5ffd9
Sanjeevi Rajan <>
2025-08-21 14:08:46 +0530

    Minor Changes.

--- server/routes/ContactsRoutes.js
+++ server/routes/ContactsRoutes.js
@@ -14,6 +14,7 @@ router.get("/contacts", GetContacts);
 router.get("/search-contact", SearchContacts);
 router.post("/create-contact", CreateContact);
 router.post("/upload-contact/", upload.single("image"), UploadImage);
+router.get("/get-contact-images/:userId", )
 router.put("/update-contact/:id", UpdateContact);
 router.delete("/delete-contact/:id", DeleteContact);
 router.post("/add-event-existing-contact/:contactId", AddEventToExistingContact);
